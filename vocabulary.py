e4d3d04109393309b0b54263b8c31dea9e19c95c
ipolyakov <igor.v.polyakov@gmail.com>
2016-10-25 10:41:37 +0300

    Started SRS on frequency list.

--- vocabulary.py
+++ vocabulary.py
@@ -1,6 +1,8 @@
 class Vocabulary:
-	def __init__(self, corpusParams):
-		return
+	def __init__(self, corpora):
+		self.corpora = corpora
 
 	def calculate(self):
+		N_QUESTIONS = 50
+		self.corpora.words()
 		return
