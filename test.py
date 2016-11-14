d998af1da8909149dc4a793b10600cd09f3f3240
ipolyakov <igor.v.polyakov@gmail.com>
2016-11-14 10:56:54 +0300

    Implementing generation of sample vocabulary basing on a frequency list.

--- test.py
+++ test.py
@@ -1,31 +1,39 @@
 import matplotlib.pyplot as plt
+import numpy
 import random
 import string
 import unittest
 
-from wordfreq import top_n_list
+from wordfreq import top_n_list, word_frequency
 
 from vocabulary import Vocabulary
 
 class MockCorpora:
 	def __init__(self, nWords):
 		self.__nWords = nWords
+		self.__words = top_n_list('en', self.__nWords, wordlist='large')
+		print(str(self.__words))
+		self.__frequencies = numpy.array([word_frequency(w, 'en') for w in self.__words])
 
 	def words(self):
-		return top_n_list('en', self.__nWords, wordlist='large')
+		return self.__words
 
 	def frequencyClasses(self):
 		return [0] * self.__nWords
 
+	def probabilities(self):
+		return self.__frequencies / sum(self.__frequencies)
+
 def measurement(nKnownWords, nUnknownWords):
 	corpora = MockCorpora(nKnownWords + nUnknownWords)
-	# say if one knows every 4th word in the entire vocabulary
-	# that means if word1 appears twice more frequent that word2 in corpora
-	# then probability of one knowing it should be twice higher as well
+	# We're using theoretical distribution of real person's vocabulary
+	# it would be interesting to test it later against real vocabularies
+	# but for now let's proceed with it
+	knownWords = set(numpy.random.choice(corpora.words(), p=corpora.probabilities()))
 	v = Vocabulary(corpora)
 	questions = v.getQuestions()
 	for q in questions:
-		# TODO: how to specify which words do I know
+		print(q.word)
 		q.setAnswer(q.word in knownWords)
 	return v.calculate(questions)
 
